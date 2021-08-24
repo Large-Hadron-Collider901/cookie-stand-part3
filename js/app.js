7476e8e2752b6698dbf203fbf053a48706544a25
Skye M <skyemc608@gmail.com>
2021-08-24 14:36:57 -0500

    changed styling

--- js/app.js
+++ js/app.js
@@ -44,25 +44,9 @@ const Product = function (filePath, name) {
 Product.allProducts = [];
 
 function generateCatalog() {
-    new Product("assets/bag.jpg", "Bag");
-    new Product("assets/banana.jpg", "Banana");
-    new Product("assets/bathroom.jpg", "Bathroom");
-    new Product("assets/boots.jpg", "Boots");
-    new Product("assets/breakfast.jpg", "Breakfast");
-    new Product("assets/bubblegum.jpg", "Bubblegum");
-    new Product("assets/chair.jpg", "Chair");
-    new Product("assets/cthulhu.jpg", "Cthulhu");
-    new Product("assets/dog-duck.jpg", "Dog-Duck");
-    new Product("assets/dragon.jpg", "Dragon");
-    new Product("assets/pen.jpg", "Pen");
-    new Product("assets/pet-sweep.jpg", "Pet Sweep");
-    new Product("assets/scissors.jpg", "Scissors");
-    new Product("assets/shark.jpg", "Shark");
-    new Product("assets/sweep.png", "Sweep");
-    new Product("assets/tauntaun.jpg", "Taun-Taun");
-    new Product("assets/unicorn.jpg", "Unicorn");
-    new Product("assets/water-can.jpg", "Water Can");
-    new Product("assets/wine-glass.jpg", "Wine Glass");
+    new Product("assets/bag.jpg", "Box of Pat's Salmon Cookies (One Dozen) $11.99");
+    new Product("assets/banana.jpg", "Pat's Salmon Cookies T-Shirt $20.16");
+    new Product("assets/bathroom.jpg", "Salmon Cookie Cutter $5.97");
 }
 
 // Initialize the app by creating the big list of products with images and names
